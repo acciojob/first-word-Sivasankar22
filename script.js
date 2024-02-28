a4fd0263ea87da64aa770affccfa36834db23422
Sivasankar22 <83053496+Sivasankar22@users.noreply.github.com>
2024-02-28 23:01:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function firstWord(s) {
-    // Split the string by whitespace characters and return the first word
+    // Trim the input string to remove leading and trailing whitespace
+    s = s.trim();
+    
+    // Split the trimmed string by whitespace characters and return the first word
     return s.split(' ')[0];
 }
 
